f06ac058661b4b1b2c5b1569bbd5f9c3d6e6ff10
Robert M Ricci <rmricci121@gmail.com>
2019-10-19 09:45:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,8 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-  superheros =["spider-man", "deadpool", "iron-man", "Captain america"]
-  superheros.sort
+  
+  array.sort
 end
 
 def using_reverse(array)
